082d0cbb962d074678f02323fd82b0dc3717cf3b
Botond Bánhidi <banhidi.botond@blathy.info>
2024-10-03 16:15:14 +0200

    Valtoztatasok idk

--- ConsoleButton/Program.cs
+++ ConsoleButton/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        string[] menuItems = { "Belépés", "Regisztráció", "Exit" };
+        string[] menuItems = { "Rajzolás", "Mentés", "Mentett Rajz", "Kilépés" };
         int selectedIndex = 0;
 
         int maxLength = 0;
@@ -16,11 +16,15 @@ class Program
             }
         }
 
-        while (true)
+        do
         {
             Console.Clear();
             int windowWidth = Console.WindowWidth;
-            int padding = (windowWidth - maxLength - 6) / 2; 
+            int windowHeight = Console.WindowHeight;
+            int padding = (windowWidth - maxLength - 6) / 2;
+            int topPadding = (windowHeight - (menuItems.Length * 3)) / 2;
+
+            Console.SetCursorPosition(0, topPadding);
 
             for (int i = 0; i < menuItems.Length; i++)
             {
@@ -60,10 +64,95 @@ class Program
                 case ConsoleKey.Enter:
                     Console.Clear();
                     Console.WriteLine($"Selected: {menuItems[selectedIndex]}");
+                    if (selectedIndex == 0)
+                    {
+                        RunFirstOptionCode();
+                    }
                     return;
             }
-        }
+        } while (true);
+    }
+
+    static void RunFirstOptionCode()
+    {
+
+        int x = 0, y = 0;
+        ConsoleKey gomb;
+        int ww = Console.WindowWidth;
+        int wh = Console.WindowHeight;
+        bool isRunning = true;
+        char currentChar = '█';
+
+        do
+        {
+            Console.SetCursorPosition(x, y);
+            gomb = Console.ReadKey(true).Key;
+
+            if (gomb == ConsoleKey.UpArrow && y > 0) y--;
+            if (gomb == ConsoleKey.DownArrow && y < wh - 1) y++;
+            if (gomb == ConsoleKey.LeftArrow && x > 0) x--;
+            if (gomb == ConsoleKey.RightArrow && x < ww - 1) x++;
+
+
+            switch (gomb)
+            {
+                case ConsoleKey.D1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case ConsoleKey.D2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+
+                case ConsoleKey.D3:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+
+                case ConsoleKey.D4:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case ConsoleKey.D5:
+                    currentChar = '▓';
+                    break;
+
+                case ConsoleKey.D6:
+                    currentChar = '▒';
+                    break;
+
+                case ConsoleKey.D7:
+                    currentChar = '░';
+                    break;
+
+                case ConsoleKey.Spacebar:
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(currentChar);
+                    break;
+            }
+
+
+            if (gomb == ConsoleKey.Tab)
+            {
+                while (true)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write('█');
+
+                    gomb = Console.ReadKey(true).Key;
+
+                    if (gomb == ConsoleKey.UpArrow && y > 0) y--;
+                    if (gomb == ConsoleKey.DownArrow && y < wh - 1) y++;
+                    if (gomb == ConsoleKey.LeftArrow && x > 0) x--;
+                    if (gomb == ConsoleKey.RightArrow && x < ww - 1) x++;
+
+                    if (gomb == ConsoleKey.Spacebar) break;
+                }
+            }
+
+            if (gomb == ConsoleKey.Escape)
+            {
+                isRunning = false;
+            }
+
+        } while (isRunning);
     }
 }
-
-//gomb korberajzolas egysegesen, kivalasztott gomb szine csak rajta
